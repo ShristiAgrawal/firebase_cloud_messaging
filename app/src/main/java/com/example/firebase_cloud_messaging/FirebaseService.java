8b9fce8c4ef837562297f57fdad1af6809ad0d03
ShristiAgrawal <agrawalshristi2@gmail.com>
2019-05-25 14:53:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase_cloud_messaging/FirebaseService.java
@@ -0,0 +1,24 @@
+package com.example.firebase_cloud_messaging;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FirebaseService extends FirebaseInstanceIdService
+{
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        //now we will have the token
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        //for now we are displaying the token in the log
+        //copy it as this method is called only when the new token is generated
+        //and usually new token is only generated when the app is reinstalled or the data is cleared
+        Log.d("MyRefreshedToken", token);
+    }
+
+}
---
+++ app/src/main/java/com/example/firebase_cloud_messaging/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.firebase_cloud_messaging;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel("c1","channel_1", importance);
+            mChannel.setDescription("channel1111");
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        /*
+         * Displaying a notification locally
+         */
+        NotificatinManager.getInstance(this).displayNotification("Greetings", "Hello how are you?");
+    }
+}
